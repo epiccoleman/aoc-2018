6b23b8ef0b6d35210ea90140d2b8b0616bc98998
Eric Coleman <eric@epiccoleman.com>
2018-12-10 13:02:08 -0500

    some refactors

--- day-09/MarbleMania.cpp
+++ day-09/MarbleMania.cpp
@@ -3,24 +3,13 @@
 #include <algorithm>
 
 MarbleMania::MarbleMania(int players, int marbles)
-  : current_index(0),
+  : current_player(0),
     current_value(1),
-    current_player(0),
     player_count(players),
     marble_count(marbles),
     marbles { 0 },
     scores(players)
 {}
-
-void MarbleMania::rotate_by_num(std::vector<int>& v, int n) {
-  for (int i = 0; i < abs(n); i++ ){
-    if (n < 0){
-      std::rotate(v.begin(), v.begin() + v.size() - 1, v.end());
-    } else {
-      std::rotate(v.begin(), v.begin() + 1, v.end());
-    }
-  }
-}
 
 void MarbleMania::rotate_by_num(std::list<int>& v, int n) {
   for (int i = 0; i < abs(n); i++ ){
@@ -41,7 +30,6 @@ void MarbleMania::take_turn() {
 
     rotate_by_num(marbles, -7);
 
-    if(marbles.front() < 0) std::cerr << "wtf at" << current_value;
     scores[current_player] += marbles.front();
     marbles.pop_front();
   } else {
@@ -58,7 +46,6 @@ void MarbleMania::take_turn() {
 
 void MarbleMania::run_game() {
   for(int i = 1; i <= marble_count; i++){
-    // if( i % 1000 == 0) std::cerr << "boy howdy i'm doin " << i << std::endl;
     take_turn();
   }
 }
--- day-09/MarbleMania.h
+++ day-09/MarbleMania.h
@@ -10,19 +10,19 @@ public:
   MarbleMania(int players, int marbles);
   void run_game();
 
-  void take_turn();
   unsigned int high_score();
 
-  int current_index;
-  int current_value;
+  static void rotate_by_num(std::list<int>& v, int n);
+
+private:
+  void take_turn();
+
   int current_player;
+  int current_value;
   int player_count;
   int marble_count;
   std::list<int> marbles;
   std::vector<unsigned int> scores;
-
-  static void rotate_by_num(std::vector<int>& v, int n);
-  static void rotate_by_num(std::list<int>& v, int n);
 };
 
 #endif
